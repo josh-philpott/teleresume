57c17b6d2f1175662f03a2f412035548697e0096
Josh Philpott <jphil529@gmail.com>
2014-01-24 23:23:55 -0500

    ...

--- run.py
+++ run.py
@@ -159,6 +159,5 @@ def handle_key():
 	return str(resp)
 		
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True, port=port)
-	
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
